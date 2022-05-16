bea52f1c014be8133faee64146d88b028d2dcab1
ralph27 <maaloulyralph@gmail.com>
2022-05-16 10:38:34 +0300

    implementing metamask authentication and balance of user

--- client/src/screens/Navbar.js
+++ client/src/screens/Navbar.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/main.css";
 import { FaBorderNone, FaRegGem } from "react-icons/fa";
 import { FaGem } from "react-icons/fa";
+import { connectWallet } from "../utils/CrowdfundInteract";
+import { balanceOf } from "../utils/ERC20Interact";
 
 function Navbar(props) {
+  const [wallet, setWallet] = useState("");
+  const [balance, setBalance] = useState(0);
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+        } else {
+          setWallet("");
+        }
+      });
+    }
+  }
+
+  const getBalance = async () => {
+    if (wallet) {
+      const res = await balanceOf(wallet)
+      console.log(res);
+    } else {
+      console.log("Connect Wallet");
+    }
+  }
+
+  
+  const handleConnect = async () => {
+    const res = await connectWallet();
+    setWallet(res[0]);
+  }
+
+  const formatAddress = () => {
+    return `${String(wallet).substring(0, 6)}...${String(wallet).substring(38)}`
+  }
+  
+  useEffect(() => {
+    ( async () => {
+      await handleConnect();
+      addWalletListener();
+      getBalance();
+    }
+    )();
+  }, [wallet]);
+
+
   return (
     <nav className="navbar">
       <div className="logo-container">
@@ -21,9 +67,9 @@ function Navbar(props) {
       <div className="wallet-container">
         <div className="wallet">
           <FaRegGem color="#fff" fontSize="1.5em" />
-          <span className="balance">2.987 GMS</span>
+          <span className="balance">{balance} GMS</span>
         </div>
-        <div className="connect-btn">Connect Wallet</div>
+        <div className="connect-btn" onClick={handleConnect}>{wallet ? formatAddress() : "Connect Wallet"}</div>
       </div>
     </nav>
   );
